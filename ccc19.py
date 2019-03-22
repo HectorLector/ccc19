d696ad1f8871695bc6f046cc78178d976bbef67a
David Seywald <d.seywald@student.tugraz.at>
2019-03-22 15:11:45 +0100

    Write output

--- ccc19.py
+++ ccc19.py
@@ -5,5 +5,11 @@ def main():
 
 
 
+def write_out_file(file_name, x, y):
+    outfile = open(file_name, "w+")
+    outfile.write(str(x) + " " + str(y))
+    outfile.close()
+
+
 if __name__== "__main__":
   main()
